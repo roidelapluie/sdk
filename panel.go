83fb1ad3fd5e99d53c276ac961705fda92819da7
Julien Pivotto <roidelapluie@inuits.eu>
2021-11-23 16:49:07 +0100

    Omitempty transformations
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- panel.go
+++ panel.go
@@ -74,7 +74,7 @@ type (
 			Y *float64 `json:"y,omitempty"`
 		} `json:"gridPos,omitempty"`
 		Height           interface{} `json:"height,omitempty"` // general
-		Transformations  interface{} `json:"transformations"`
+		Transformations  interface{} `json:"transformations,omitempty"`
 		HideTimeOverride *bool       `json:"hideTimeOverride,omitempty"`
 		ID               uint        `json:"id"`
 		IsNew            bool        `json:"isNew"`
